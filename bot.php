b58d6c731880538a3c10f6a3a74540aa55852d78
numpapickmaker <numpapickmaker@users.noreply.github.com>
2017-08-22 01:40:00 +0700

    test

--- bot.php
+++ bot.php
@@ -1,7 +1,7 @@
 <?php
 include ('line-bot-api/php/line-bot.php');
-$channelSecret = '551ec4........3cff0';
-$access_token  = '2og9ogezC..... W5ZUEQQdB04t89/1O/w1cDnyilFU=';
+$channelSecret = 'U306cd00872315c9a853169211616fd59';
+$access_token  = '2uqo5ucAcfrmOpw/3eaZFd6acQsNKYS1eqq7AK/aq6+tG9qGgetZbduYbg7pydy1nRWFJVGH5xXBJyB9Rag7mfL34PsnR8Qzmrr4JVBQBRTR0Q+R3gocjfm67V7v0Am9bHoUqYRCpTIIjrO7CceKdQdB04t89/1O/w1cDnyilFU=';
 $bot = new BOT_API($channelSecret, $access_token);
 	
 $bot->sendMessageNew('U39f72cc.....d460d6ddf', 'Hello World !!');
