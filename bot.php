bdc2953cc34c9b7ba0fdcc32333be38e25eed70e
numpapickmaker <numpapickmaker@users.noreply.github.com>
2017-08-22 01:17:06 +0700

    update
    update

--- bot.php
+++ bot.php
@@ -27,7 +27,7 @@ if (!is_null($events['events'])) {
 			$data = [
 
 				'replyToken' => $replyToken,
-				'messages' => ':)',
+				'messages' => $messages
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
