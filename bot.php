614b3ff802d3d026b0abc2762da23d5b4c6ebde7
numpapickmaker <numpapickmaker@users.noreply.github.com>
2017-08-26 20:12:47 +0700

    TEST

--- bot.php
+++ bot.php
@@ -22,13 +22,37 @@ $debug_dump = ob_get_clean();
 
 			// Build message to reply back
 			$messages = [
-				'type' => 'image',
-			 	'originalContentUrl'=> 'https://cdn.pixabay.com/photo/2014/11/28/22/23/alm-549333_960_720.jpg',
-			 	'previewImageUrl'=> 'https://i.imgur.com/2WCYC2V.jpg?1'
+				
+			  "type": "template",
+			  "altText": "this is a buttons template",
+			  "template": [
+			      "type": "buttons",
+			      "thumbnailImageUrl": "https://example.com/bot/images/image.jpg",
+			      "title": "Menu",
+			      "text": "Please select",
+			      "actions": [
+			          [
+			            "type": "postback",
+			            "label": "Buy",
+			            "data": "action=buy&itemid=123"
+			          ],
+			          [
+			            "type": "postback",
+			            "label": "Add to cart",
+			            "data": "action=add&itemid=123"
+			          ],
+			          [
+			            "type": "uri",
+			            "label": "View detail",
+			            "uri": "http://example.com/page/123"
+			          ]
+			      ]
+			  ]
+
+			];
 				// 'type' => 'text',
 				// 'text' => $debug_dump
 				//'text' => $text
-			];
 					//"type" => "location",
 				    // "title" => "my location",
 				    // "address" => "KMUTNB",
