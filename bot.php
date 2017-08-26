6b36e38951613d6476691b48d19cb11d2239c249
numpapickmaker <numpapickmaker@users.noreply.github.com>
2017-08-26 20:27:02 +0700

    test

--- bot.php
+++ bot.php
@@ -22,32 +22,61 @@ $debug_dump = ob_get_clean();
 
 			// Build message to reply back
 			$messages = [
-				
-			  "type"=> "template",
-			  "altText"=> "this is a buttons template",
-			  "template"=> [
-			      "type"=> "buttons",
-			      "thumbnailImageUrl"=> "https://example.com/bot/images/image.jpg",
-			      "title"=> "Menu",
-			      "text"=> "Please select",
-			      "actions"=> [
-			          [
-			            "type"=> "postback",
-			            "label"=> "Buy",
-			            "data"=> "action=buy&itemid=123"
-			          ],
-			          [
-			            "type"=> "postback",
-			            "label"=> "Add to cart",
-			            "data"=> "action=add&itemid=123"
-			          ],
-			          [
-			            "type"=> "uri",
-			            "label"=> "View detail",
-			            "uri"=> "http://example.com/page/123"
-			          ]
-			      ]
+					[
+				  "type"=> "template",
+				  "altText"=> "this is a carousel template",
+				  "template"=> [
+				      "type"=> "carousel",
+				      "columns"=> [
+				          [
+				            "thumbnailImageUrl"=> "https://example.com/bot/images/item1.jpg",
+				            "title"=> "this is menu",
+				            "text"=> "description",
+				            "actions"=> [
+				                [
+				                    "type"=> "postback",
+				                    "label"=> "Buy",
+				                    "data"=> "action=buy&itemid=111"
+				                ],
+				                [
+				                    "type"=> "postback",
+				                    "label"=> "Add to cart",
+				                    "data"=> "action=add&itemid=111"
+				                ],
+				                [
+				                    "type"=> "uri",
+				                    "label"=> "View detail",
+				                    "uri"=> "http://example.com/page/111"
+				                ]
+				            ]
+				          ],
+				          [
+				            "thumbnailImageUrl"=> "https=>//example.com/bot/images/item2.jpg",
+				            "title"=> "this is menu",
+				            "text"=> "description",
+				            "actions"=> [
+				                [
+				                    "type"=> "postback",
+				                    "label"=> "Buy",
+				                    "data"=> "action=buy&itemid=222"
+				                ],
+				                [
+				                    "type"=> "postback",
+				                    "label"=> "Add to cart",
+				                    "data"=> "action=add&itemid=222"
+				                ],
+				                [
+				                    "type"=> "uri",
+				                    "label"=> "View detail",
+				                    "uri"=> "http://example.com/page/222"
+				                ]
+				            ]
+				          ]
+				      ]
+				  ]
+				]
   
+			  
 
 			];
 				// 'type' => 'text',
