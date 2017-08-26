a61496e0985a2420f2062089a69b0bacae5779ad
numpapickmaker <numpapickmaker@users.noreply.github.com>
2017-08-26 20:41:22 +0700

    test temperature

--- bot.php
+++ bot.php
@@ -22,55 +22,28 @@ $debug_dump = ob_get_clean();
 
 			// Build message to reply back
 			$messages = [
-				
 				  "type"=> "template",
-				  "altText"=> "this is a carousel template",
+				  "altText"=> "this is a buttons template",
 				  "template"=> [
-				      "type"=> "carousel",
-				      "columns"=> [
+				      "type"=> "buttons",
+				      "thumbnailImageUrl"=> "https://example.com/bot/images/image.jpg",
+				      "title"=> "Menu",
+				      "text"=> "Please select",
+				      "actions"=> [
 				          [
-				            "thumbnailImageUrl"=> "https://example.com/bot/images/item1.jpg",
-				            "title"=> "this is menu",
-				            "text"=> "description",
-				            "actions"=> [
-				                [
-				                    "type"=> "postback",
-				                    "label"=> "Buy",
-				                    "data"=> "action=buy&itemid=111"
-				                ],
-				                [
-				                    "type"=> "postback",
-				                    "label"=> "Add to cart",
-				                    "data"=> "action=add&itemid=111"
-				                ],
-				                [
-				                    "type"=> "uri",
-				                    "label"=> "View detail",
-				                    "uri"=> "http://example.com/page/111"
-				                ]
-				            ]
+				            "type"=> "postback",
+				            "label"=> "Buy",
+				            "data"=> "action=buy&itemid=123"
 				          ],
 				          [
-				            "thumbnailImageUrl"=> "https://example.com/bot/images/item2.jpg",
-				            "title"=> "this is menu",
-				            "text"=> "description",
-				            "actions"=> [
-				                [
-				                    "type"=> "postback",
-				                    "label"=> "Buy",
-				                    "data"=> "action=buy&itemid=222"
-				                ],
-				                [
-				                    "type"=> "postback",
-				                    "label"=> "Add to cart",
-				                    "data"=> "action=add&itemid=222"
-				                ],
-				                [
-				                    "type"=> "uri",
-				                    "label"=> "View detail",
-				                    "uri"=> "http://example.com/page/222"
-				                ]
-				            ]
+				            "type"=> "postback",
+				            "label"=> "Add to cart",
+				            "data"=> "action=add&itemid=123"
+				          ],
+				          [
+				            "type"=> "uri",
+				            "label"=> "View detail",
+				            "uri"=> "http://example.com/page/123"
 				          ]
 				      ]
 				  ]
