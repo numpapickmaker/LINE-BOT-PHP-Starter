08211dfe72f4585b1df1106165226438344b28a1
numpapickmaker <numpapickmaker@users.noreply.github.com>
2017-08-26 20:35:49 +0700

    update sell

--- bot.php
+++ bot.php
@@ -1,19 +1,25 @@
 <?php
 $access_token = '2uqo5ucAcfrmOpw/3eaZFd6acQsNKYS1eqq7AK/aq6+tG9qGgetZbduYbg7pydy1nRWFJVGH5xXBJyB9Rag7mfL34PsnR8Qzmrr4JVBQBRTR0Q+R3gocjfm67V7v0Am9bHoUqYRCpTIIjrO7CceKdQdB04t89/1O/w1cDnyilFU=';
+
 // Get POST body content
-//$content = file_get_contents('php://input');
+$content = file_get_contents('php://input');
 // Parse JSON
-//$events = json_decode($content, true);
+$events = json_decode($content, true);
 // Validate parsed JSON data
-//if (!is_null($events['events'])) {
-	// Loop through each event
-	//foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format
-		//if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
-			//$text = $event['message']['text'];
-			// Get replyToken
-			//$replyToken = $event['replyToken'];
+$debug_export = var_export($events, true);
+ob_start();
+var_dump($events);
+$debug_dump = ob_get_clean();
+// if (!is_null($events['events'])) {
+// 	// Loop through each event
+// 	foreach ($events['events'] as $event) {
+// 		// Reply only when message sent is in 'text' format
+// 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+// 			// Get text sent
+// 			$text = $event+$text;
+// 			// Get replyToken
+// 			$replyToken = $event['replyToken'];
+
 			// Build message to reply back
 			$messages = [
 				
@@ -69,6 +75,9 @@ $access_token = '2uqo5ucAcfrmOpw/3eaZFd6acQsNKYS1eqq7AK/aq6+tG9qGgetZbduYbg7pydy
 				      ]
 				  ]
 			];
+				// 'type' => 'text',
+				// 'text' => $debug_dump
+				//'text' => $text
 					//"type" => "location",
 				    // "title" => "my location",
 				    // "address" => "KMUTNB",
@@ -78,14 +87,17 @@ $access_token = '2uqo5ucAcfrmOpw/3eaZFd6acQsNKYS1eqq7AK/aq6+tG9qGgetZbduYbg7pydy
 			//  'type' => 'image',
 			// 	'originalContentUrl'=> 'https://cdn.pixabay.com/photo/2014/11/28/22/23/alm-549333_960_720.jpg',
    			//  'previewImageUrl'=> 'https://i.imgur.com/2WCYC2V.jpg?1'
+
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/push';
 			$data = [
+
 				'to' => 'U306cd00872315c9a853169211616fd59',
 				'messages' => [$messages],
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
 			$ch = curl_init($url);
 			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -94,8 +106,9 @@ $access_token = '2uqo5ucAcfrmOpw/3eaZFd6acQsNKYS1eqq7AK/aq6+tG9qGgetZbduYbg7pydy
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 			$result = curl_exec($ch);
 			curl_close($ch);
+
 			echo $result . "\r\n";
-		//}
-	//}
-//}
+		
+// 	}
+// }
 echo "OK";
